2119c3c5b21b7f7426e7c93178a4a79fde5bb14a
Abdulla-Khan <abdullahkhan.smiu@gmail.com>
2024-09-05 17:51:47 +0500

    Initial Commit

---
+++ Apple Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple Framework
+//
+//  Created by Ayaz on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
